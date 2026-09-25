9c0b661ac2dc491b406d15c08c18b5923560187c
Your Name <lundbot2@gmail.com>
2026-09-26 05:06:29 +0700

    final

--- app/Http/Requests/UpdateActivityRequest.php
+++ app/Http/Requests/UpdateActivityRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateActivityRequest extends FormRequest
 {
@@ -14,11 +15,14 @@ class UpdateActivityRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => ['required', 'string', 'min: 5', 'max:100'],
-            'description' => ['required', 'string'],
+            'title' => ['required', 'string', 'min:5', 'max:100'],
+            'description' => ['nullable', 'string', 'max:1000'],
             'activity_date' => ['required', 'date'],
-            'category' => ['required', 'string', 'max:100'],
-            'status' => ['required', 'string', 'in:Planned,Ongoing,Done'],
+            'category' => ['required', 'string', 'max:50'],
+            'status' => [
+                'required',
+                Rule::in(['Planned', 'Ongoing', 'Done']),
+            ],
         ];
     }
 }
